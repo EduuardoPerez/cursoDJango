a2d43c11401880ff89e37b80114f15dca2ac2598
Eduardo <edujpp1@gmail.com>
2018-08-15 21:47:25 -0300

    Se colocaran los detalles de los cambios en este commit al volver a ver el tutorial nro 12

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from apps.mascota.views import index
+from apps.mascota.views import index, mascota_view
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='index'),
+    path('nuevo', mascota_view, name='mascota_crear'),
 ]
