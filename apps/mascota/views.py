daadab536bc94b06a4e4e4879f7fb57a4aab4281
Eduardo <edujpp1@gmail.com>
2018-08-17 18:01:15 -0300

    Se creo el html mascota_list para ser visto desde /mascota/listar que sera cargada desde la vista mascota_list. En esta se cargan en una tabla HTML todos los objetos que contienen el modelo Mascota, es decir, los que se encuentran guardados en la BD. Se realizaron todas las configuraciones necesarias en views.py, mascota_list.html y urls.py de la app mascota

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 from apps.mascota.forms import MascotaForm
+from apps.mascota.models import Mascota
 
 # Create your views here.
-
 
 def index(request):
 	return render(request, 'mascota/index.html')
@@ -16,5 +17,9 @@ def mascota_view(request):
 		return redirect('mascota:index')
 	else:
 		form = MascotaForm()
-
 	return render(request, 'mascota/mascota_form.html', {'form':form})
+
+def mascota_list(request):
+	mascota = Mascota.objects.all()
+	contexto = {'mascotas':mascota}
+	return render(request, 'mascota/mascota_list.html', contexto)
