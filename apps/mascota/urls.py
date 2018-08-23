630cade75234edcbeeaf3f08bebb7adc7df188b5
Eduardo <edujpp1@gmail.com>
2018-08-23 19:08:26 -0300

    Se agrego nuevo contenido de tutoriales anteriores en Notas.txt. Se creo el archivo iniciarEV.bat para iniciar de manera automatica el entorno virtual y se modifico el activate.bat del entorno para que devuelva a la carpeta raiz del proyecto. Se crearon las vistas basadas en clases MascotaUpdate y MascotaDelete para cumplir la funcion de las listas basadas en funciones mascota_edit y mascotadelete. Se realizaron las configuraciones necesarias en el urls.py de la app mascota para poder utilzar las nuevas vistas

--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreate
+from apps.mascota.views import index, mascota_view, mascota_list, mascota_edit, mascota_delete, MascotaList, MascotaCreate, MascotaUpdate, MascotaDelete
 
 urlpatterns = [
     path('', index, name='index'),
     path('nuevo', MascotaCreate.as_view(), name='mascota_crear'),
     path('listar', MascotaList.as_view(), name='mascota_listar'),
-    path('editar/<int:id_mascota>/', mascota_edit, name='mascota_editar'),
-    path('eliminar/<int:id_mascota>/', mascota_delete, name='mascota_eliminar'),
+    path('editar/<pk>/', MascotaUpdate.as_view(), name='mascota_editar'),
+    path('eliminar/<pk>/', MascotaDelete.as_view(), name='mascota_eliminar'),
 ]
---
+++ iniciarEV.bat
@@ -0,0 +1,3 @@
+D:
+cd D:\Cursos\codigofacilito\DJango\EV\curDJango\Scripts
+start activate.bat
