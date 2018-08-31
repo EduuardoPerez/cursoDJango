5963abe35094150566e6951c20e2d2302498de08
Eduardo <edujpp1@gmail.com>
2018-08-31 18:42:24 -0300

    Se creo el template registrar.html para hacer uso del modelo de DJango User y del form de DJango UserCreationForm y asi crear y guardar usuarios en la BD. La forma en como se guarda a traves de este form de DJango se modifico en el forms.py creado en la app usuario.

--- cursoDjango/urls.py
+++ cursoDjango/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('mascota/', include(('apps.mascota.urls', 'mascota'), namespace="mascota")),
     path('adopcion/', include(('apps.adopcion.urls', 'adopcion'), namespace="adopcion")),
+    path('usuario/', include(('apps.usuario.urls', 'usuario'), namespace="usuario")),
 ]
