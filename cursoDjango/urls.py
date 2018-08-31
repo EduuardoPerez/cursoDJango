22a5741f666dc2ecbdb2dec2f72868af18e67db1
Eduardo <edujpp1@gmail.com>
2018-08-31 19:56:04 -0300

    Se hizo uso de la vista LoginView para realizar un login y colocarlo como pagina principal de la web-app, la configuracion de esta vista fue un poco distinta a la del curso dado que en el curso se trabaja con una version anterior a la DJango 2.1 que es la que se esta utilizando. En Notas.txt se indican que configuraciones se realizaron

--- cursoDjango/urls.py
+++ cursoDjango/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mascota/', include(('apps.mascota.urls', 'mascota'), namespace="mascota")),
     path('adopcion/', include(('apps.adopcion.urls', 'adopcion'), namespace="adopcion")),
     path('usuario/', include(('apps.usuario.urls', 'usuario'), namespace="usuario")),
+    path('', auth_views.LoginView.as_view(template_name='index.html'), name="login"),
 ]
