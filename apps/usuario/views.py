92302a18a09f3ea23c03ea8854c9bafa974f3e72
Eduardo <edujpp1@gmail.com>
2018-09-01 20:28:08 -0300

    Fin del curso. Se serializaron objetos haciendo uso del paquete de DJango RestFramework. Se creo una url (usuario/api) donde se muestra el nombre y email de los usuarios que se encuentra en el modelo User. Esto gracias al framework mencionado.

--- apps/usuario/views.py
+++ apps/usuario/views.py
@@ -1,10 +1,14 @@
-#from django.shortcuts import render
+import json
+from rest_framework.views import APIView
+
+from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 from apps.usuario.forms import RegistroForm
+from apps.usuario.serializers import UserSerializer
 
 # Create your views here.
 
@@ -13,3 +17,12 @@ class RegistroUsuario(CreateView):
 	template_name = 'usuario/registrar.html'
 	form_class = RegistroForm
 	success_url = reverse_lazy('mascota:mascota_listar')
+
+class UserAPI(APIView):
+	serializer = UserSerializer
+
+	def get(self, request, format=None):
+		lista = User.objects.all()
+		response = self.serializer(lista, many=True)
+
+		return HttpResponse(json.dumps(response.data), content_type='application/json')
